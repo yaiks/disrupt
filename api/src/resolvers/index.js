6b80c8d55a0d5ce56d4e33b2b68dc4fc4d28b621
Ricardo <ricardoshhan@gmail.com>
2019-09-01 22:25:00 -0300

    separating frontend from backend, each in a repository

--- api/src/resolvers/index.js
+++
@@ -1,5 +0,0 @@
-import userResolvers from "./user";
-import productResolvers from "./product";
-import commentResolvers from "./comment";
-
-export default [userResolvers, productResolvers, commentResolvers];
